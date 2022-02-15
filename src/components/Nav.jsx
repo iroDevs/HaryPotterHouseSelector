a44c02de223aa6ac8b5fe5f4a62034080c78b8a3
iroDevs <ironegrolol@gamil.com>
2022-02-15 19:24:24 -0300

    pag home feita

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+export default function Nav() {
+  return (
+         <nav>
+      <ul>
+        <li><a href='https://www.linkedin.com/in/pedro-2001/'>Meu Linkedin</a></li>
+        <li><a href='https://github.com/iroDevs'>Meu GitHub</a></li>
+        <li id="btn-luz">Litgh Mode</li>
+      </ul>
+      </nav>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import Button from '../components/Buttons'
 import ChapeuSeletor from '../images/chapeuSeletor.png'
+import Nav from '../components/Nav'
 export default function Home () {
   return (
     <div className="container-fluid Home">
-      <nav>
-      <ul>
-        <li><a href='https://www.linkedin.com/in/pedro-2001/'>Meu Linkedin</a></li>
-        <li><a href='https://github.com/iroDevs'>Meu GitHub</a></li>
-        <li id="btn-luz">Litgh Mode</li>
-      </ul>
-      </nav>
-
+      <Nav />
       <div className='title-menu'>
          <img className="imgChapeu" src={ChapeuSeletor}></img>
       <h1>Chapéu Selector</h1>
