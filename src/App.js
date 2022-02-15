ebd900c7172b4f42f3ed8ade406f2fccc4c64a9a
iroDevs <ironegrolol@gamil.com>
2022-02-15 18:04:01 -0300

    criando animaçoes com chapeu e arrumando links git e linkedin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-import logo from './logo.svg'
+
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Button from '../components/Buttons'
+import ChapeuSeletor from '../images/chapeuSeletor.png'
 export default function Home () {
   return (
     <div className="container-fluid Home">
       <nav>
       <ul>
-        <li>Meu GitHub</li>
-        <li>Meu Linkedin</li>
+        <li><a href='https://www.linkedin.com/in/pedro-2001/'>Meu Linkedin</a></li>
+        <li><a href='https://github.com/iroDevs'>Meu GitHub</a></li>
         <li id="btn-luz">Litgh Mode</li>
       </ul>
       </nav>
+
       <div className='title-menu'>
+         <img className="imgChapeu" src={ChapeuSeletor}></img>
       <h1>Chapéu Selector</h1>
       <Button></Button>
       </div>
